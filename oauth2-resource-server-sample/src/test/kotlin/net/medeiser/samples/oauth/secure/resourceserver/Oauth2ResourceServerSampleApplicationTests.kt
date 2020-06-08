c7f5b8d08e5401bd4570476daead76689d45dd0b
Sergio Medeiros <smpme.dev@gmail.com>
2020-06-08 18:16:45 +0100

    initial commit

---
+++ oauth2-resource-server-sample/src/test/kotlin/net/medeiser/samples/oauth/secure/resourceserver/Oauth2ResourceServerSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.medeiser.samples.oauth.secure.resourceserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Oauth2ResourceServerSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ oauth2-webclient-sample/src/test/kotlin/net/medeiser/samples/oauth/secure/oauth2webclientsample/Oauth2WebclientSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.medeiser.samples.oauth.secure.oauth2webclientsample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Oauth2WebclientSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
