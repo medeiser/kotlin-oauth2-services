15c67a5e61bea61cfaa99e4e7363f7bb6f1827ce
Sergio Medeiros <smpme.dev@gmail.com>
2020-06-15 08:43:58 +0100

    Client application correctly configured

--- oauth2-webclient-sample/src/main/kotlin/net/medeiser/samples/oauth/secure/oauth2webclientsample/config/WebClientConfig.kt
+++ oauth2-webclient-sample/src/main/kotlin/net/medeiser/samples/oauth/secure/oauth2webclientsample/config/WebClientConfig.kt
@@ -2,9 +2,12 @@ package net.medeiser.samples.oauth.secure.oauth2webclientsample.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.DefaultReactiveOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction
-import org.springframework.security.oauth2.client.web.server.UnAuthenticatedServerOAuth2AuthorizedClientRepository
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository
 import org.springframework.web.reactive.function.client.WebClient
 
 
@@ -12,12 +15,24 @@ import org.springframework.web.reactive.function.client.WebClient
 class WebClientConfig {
 
     @Bean
-    fun webClient(clientRegistrations: ReactiveClientRegistrationRepository): WebClient {
-        val oauth = ServerOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrations, UnAuthenticatedServerOAuth2AuthorizedClientRepository())
-        oauth.setDefaultClientRegistrationId("custom")
+    fun webClient(authorizedClientManager: ReactiveOAuth2AuthorizedClientManager): WebClient {
+        val oauth2Client = ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
+        oauth2Client.setDefaultClientRegistrationId("custom")
         return WebClient.builder()
-                .filter(oauth)
+                .filter(oauth2Client)
                 .build()
     }
 
+    @Bean
+    fun authorizedClientManager(
+            clientRegistrationRepository: ReactiveClientRegistrationRepository,
+            authorizedClientRepository: ServerOAuth2AuthorizedClientRepository): ReactiveOAuth2AuthorizedClientManager {
+        val authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build()
+        val authorizedClientManager = DefaultReactiveOAuth2AuthorizedClientManager(
+                clientRegistrationRepository, authorizedClientRepository)
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+        return authorizedClientManager
+    }
 }
