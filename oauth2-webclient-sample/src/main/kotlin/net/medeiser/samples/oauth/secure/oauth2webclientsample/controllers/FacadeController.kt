7642a41e95c68a8e3ab864a8e6714eeca9d685ec
Sergio Medeiros <smpme.dev@gmail.com>
2020-06-15 08:43:58 +0100

    Client application correctly configured

--- oauth2-webclient-sample/src/main/kotlin/net/medeiser/samples/oauth/secure/oauth2webclientsample/controllers/FacadeController.kt
+++ oauth2-webclient-sample/src/main/kotlin/net/medeiser/samples/oauth/secure/oauth2webclientsample/controllers/FacadeController.kt
@@ -1,4 +1,4 @@
-package net.medeiser.samples.oauth.secure.oauth2webclientsample.api.controllers
+package net.medeiser.samples.oauth.secure.oauth2webclientsample.controllers
 
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
